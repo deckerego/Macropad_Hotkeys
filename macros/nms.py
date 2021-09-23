98a397086e495fca563def6bcc129195c5877628
John Ellis <john@deckerego.net>
2021-09-23 11:32:13 -0400

    Better NMS Sky colors

--- macros/nms.py
+++ macros/nms.py
@@ -11,16 +11,16 @@ app = {
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x544408, 'G Map', [Keycode.M]),
-        (0x000000, 'Boost', [Keycode.LEFT_SHIFT]),
+        (0x095E06, 'Boost', [Keycode.LEFT_SHIFT]),
         (0x540908, 'Pulse', [Keycode.SPACE]),
         # 2nd row ----------
         (0x000754, 'Prev ', [Keycode.PERIOD]),
         (0x540908, 'Target', [Mouse(MouseCode.MIDDLE_BUTTON)]),
         (0x000754, 'Next ', [Keycode.COMMA]),
         # 3rd row ----------
-        (0x080F54, 'Scan ', [Keycode.C]),
-        (0x080F54, 'Light', [Keycode.T]),
-        (0x080F54, 'HUD  ', [Keycode.H]),
+        (0x047F91, 'Scan ', [Keycode.C]),
+        (0x505050, 'Light', [Keycode.T]),
+        (0x6B0373, 'HUD  ', [Keycode.H]),
         # 4th row ----------
         (0x095E06, 'Quick', [Keycode.X]),
         (0x000000, '     ', []),
