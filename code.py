e418251c92d4cdf53bef8b8833073630368f872b
John Ellis <john@deckerego.net>
2021-09-27 09:58:00 -0400

    Refactored display logic

--- code.py
+++ code.py
@@ -2,64 +2,29 @@
 
 import os
 import time
-import displayio
-import terminalio
-from adafruit_display_shapes.rect import Rect
-from adafruit_display_text import label
 from adafruit_macropad import MacroPad
 from app import App
 from consumer import Toolbar
 from mouse import Mouse
 from sleeper import Sleep
 from keyboard import Keyboard
+from display import Display
 
 MACRO_FOLDER = '/macros'
 
 macropad = MacroPad()
-macropad.display.auto_refresh = False
 macropad.pixels.auto_write = False
-group = None
-
-def init_display():
-    global group
-    group = displayio.Group()
-    for key_index in range(12):
-        x = key_index % 3
-        y = key_index // 3
-        group.append(
-            label.Label(terminalio.FONT,
-                text='',
-                color=0xFFFFFF,
-                anchored_position=((macropad.display.width - 1) * x / 2,
-                macropad.display.height - 1 - (3 - y) * 12),
-                anchor_point=(x / 2, 1.0)
-            )
-        )
-    group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
-    group.append(
-        label.Label(
-            terminalio.FONT,
-            text='',
-            color=0x000000,
-            anchored_position=(macropad.display.width//2, -2),
-            anchor_point=(0.5, 0.0)
-        )
-    )
-    macropad.display.show(group)
+screen = Display(macropad)
 
 def switch(app):
-    global group
     macropad.keyboard.release_all()
-    group[13].text = app.name   # Application name
+    screen.setApp(app)
     for i in range(12):
         if i < len(app.macros): # Key in use, set label + LED color
             macropad.pixels[i] = app.macros[i][0]
-            group[i].text = app.macros[i][1]
         else:  # Key not in use, no label or LED
             macropad.pixels[i] = 0
-            group[i].text = ''
     macropad.pixels.show()
-    macropad.display.refresh()
 
 def make_key(item):
     if isinstance(item, Toolbar):
@@ -71,7 +36,7 @@ def make_key(item):
     else:
         return Keyboard(item)
 
-init_display()
+screen.initialize()
 apps = []
 files = os.listdir(MACRO_FOLDER)
 for filename in files:
@@ -84,8 +49,7 @@ for filename in files:
             pass
 
 if not apps:
-    group[13].text = 'NO MACRO FILES FOUND'
-    macropad.display.refresh()
+    display.setText('NO MACRO FILES FOUND')
     quit()
 
 apps.sort(key=lambda m:m.order)
@@ -101,9 +65,7 @@ while True:
         break
     except OSError as err:
         print(err)
-        init_display()
-        group[13].text = 'NO USB CONNECTION'
-        macropad.display.refresh()
+        display.setText('NO USB CONNECTION')
         time.sleep(5000)
 
 while True:
@@ -133,17 +95,11 @@ while True:
             macropad.pixels.show()
         elif key_number is 12:
             if not sleeping:
+                display.sleep()
                 macropad.pixels.fill((0, 0, 0))
                 macropad.pixels.show()
-                macropad.display.auto_brightness = False
-                macropad.display.brightness = 0
-                macropad.display.show(displayio.Group())
-                macropad.display.refresh()
             else:
-                macropad.display.brightness = 1
-                macropad.display.auto_brightness = True
-                macropad.display.show(group)
-                macropad.display.refresh()
+                display.resume()
                 switch(apps[app_index])
             sleeping = not sleeping
 
