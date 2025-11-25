a929a860ecf8211cac11449a76670c4383cb4f26
John Ellis <532789+deckerego@users.noreply.github.com>
2025-11-25 12:55:57 -0500

    Allow a macro to be executed on launch

--- code.py
+++ code.py
@@ -6,6 +6,12 @@ from display import Display
 from pixels import Pixels
 from sleep import Sleep
 
+KEY_LAUNCH = -1
+KEY_ENC_BUTTON = 12
+KEY_ENC_LEFT = 13
+KEY_ENC_RIGHT = 14
+MAX_KEYS = 14
+MAX_LEDS = 12
 MACRO_FOLDER = '/macros'
 SLEEP_AFTER = 300 # Dim the display after 5 minutes
 
@@ -14,7 +20,7 @@ last_position = macropad.encoder
 last_time_seconds = time.monotonic()
 sleep_remaining = SLEEP_AFTER
 macro_changed = False
-app_index = 0
+current_app = None
 
 state = {
     "macropad": macropad,
@@ -30,6 +36,23 @@ def elapsed_seconds():
     elapsed_seconds = current_seconds - last_time_seconds
     last_time_seconds = current_seconds
     return elapsed_seconds
+
+def set_app(index):
+    global current_app
+    current_app = apps[index]
+    state["macropad"].keyboard.release_all()
+    state["screen"].setApp(current_app)
+    state["pixels"].setApp(current_app)
+
+def get_sequence(key):
+    global current_app
+    if key == KEY_LAUNCH:
+        return current_app.launch[2] if current_app.launch else None
+    try: # No such sequence for this key
+        return current_app.macros[key][2] if key <= MAX_KEYS else []
+    except (IndexError) as err:
+        print("Couldn't find sequence for key number ", key)
+        return None
 
 # Load available macros
 state["screen"].initialize()
@@ -48,10 +71,9 @@ except OSError as err:
         pass
 
 # Prep before the run loop
-state["screen"].setApp(apps[0])
-state["pixels"].setApp(apps[0])
+set_app(0)
 
-while True:
+while True: # Event loop
     position = state["macropad"].encoder
     state["macropad"].encoder_switch_debounced.update()
     pressed = False
@@ -67,34 +89,26 @@ while True:
     if position != last_position and state["macropad"].encoder_switch:
         macro_changed = True
         last_position = position
-        app_index = position % len(apps)
-        state["macropad"].keyboard.release_all()
-        state["screen"].setApp(apps[app_index])
-        state["pixels"].setApp(apps[app_index])
+        set_app(position % len(apps))
         continue # Changing macros, not a keypress event
     elif macro_changed and state["macropad"].encoder_switch_debounced.released:
         macro_changed = False
-        continue # We've changed macros, ignore encoder press
+        key_number = KEY_LAUNCH
+        rotated = True
     elif state["macropad"].encoder_switch_debounced.released:
-        key_number = 12
+        key_number = KEY_ENC_BUTTON
         pressed = state["macropad"].encoder_switch_debounced.released
     elif position != last_position:
-        key_number = 13 if position < last_position else 14
+        key_number = KEY_ENC_LEFT if position < last_position else KEY_ENC_RIGHT
         last_position = position
         rotated = True
     else:
         event = state["macropad"].keys.events.get()
-        if not event or event.key_number >= len(apps[app_index].macros):
+        if not event or event.key_number >= len(current_app.macros):
             if state["sleeping"]: time.sleep(1.0) # Low power mode
             continue # No key events, or no corresponding macro, resume loop
         key_number = event.key_number
         pressed = event.pressed
-
-    try: # No such sequence for this key
-        sequence = apps[app_index].macros[key_number][2] if key_number <= 14  else []
-    except (IndexError) as err:
-        print("Couldn't find sequence for key number ", key_number)
-        sequence = None
 
     # Wake up if there is a key event while sleeping
     if state["sleeping"] and (pressed or rotated):
@@ -102,8 +116,9 @@ while True:
         continue
     sleep_remaining = SLEEP_AFTER
 
-    if sequence and (pressed or rotated):
-        if not state["sleeping"] and key_number < 12:
+    sequence = get_sequence(key_number)
+    if sequence and (rotated or pressed):
+        if not state["sleeping"] and key_number < MAX_LEDS:
             state["pixels"].highlight(key_number, 0xFFFFFF)
 
         if type(sequence) is list:
@@ -118,7 +133,7 @@ while True:
         else: # We just have a single command to execute
             keyfactory.get(sequence).press(state)
                 
-    if sequence and (not pressed or rotated):
+    if sequence and (rotated or not pressed):
         if type(sequence) is list: 
             for item in sequence:
                 if type(item) is not list: # Release any still-pressed key combinations
@@ -126,5 +141,5 @@ while True:
                 # Macro key cobinations should already have been released
         else: # Release any still-pressed single commands
             keyfactory.get(sequence).release(state)
-        if not state["sleeping"] and key_number < 12: # No pixel for encoder button
+        if not state["sleeping"] and key_number < MAX_LEDS: # No pixel for encoder button
             state["pixels"].reset(key_number)
