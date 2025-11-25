65dcc6e1793ec915298427bb115824a8108a49fc
John Ellis <532789+deckerego@users.noreply.github.com>
2025-11-25 12:56:22 -0500

    More useful Resolve navigation shortcuts

--- macros/examples/resolve_cut.py
+++ macros/examples/resolve_cut.py
@@ -1,32 +1,36 @@
 # MACROPAD Hotkeys: DaVinci Resolve Cut Page
 
 from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from consumer import Toolbar
 from sleep import Sleep
 
 app = {
     'name' : 'DV Resolve [Cut]',
     'order': 0,
+    'launch': 
+        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.THREE]),
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x000000, '<<', 'j'),
-        (0x000000, '>||', 'k'),
-        (0x000000, '>>', 'l' ),
+        (0x663F00, 'Vol -', Toolbar(ConsumerControlCode.VOLUME_DECREMENT)),
+        (0x660556, 'Mute', Toolbar(ConsumerControlCode.MUTE)),
+        (0x663F00, 'Vol +', Toolbar(ConsumerControlCode.VOLUME_INCREMENT)),
         # 2nd row ----------
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
+        (0x663F00, '<<', 'j'),
+        (0x660556, '>||', Keycode.SPACE),
+        (0x663F00, '>>', 'l' ),
         # 3rd row ----------
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
+        (0x663F00, 'Prev', Keycode.UP_ARROW),
+        (0x660556, ' [*] ', 'k'),
+        (0x663F00, 'Next', Keycode.DOWN_ARROW),
         # 4th row ----------
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
+        (0x663F00, 'In', 'i'),
+        (0x660556, 'Clear', 'x'),
+        (0x663F00, 'Out', 'o'),
         # Rotary Encoder ---
         (0x000000, None, Sleep()),
-        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW]),
-        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW]),
+        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW], [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW], [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW]),
+        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW], [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW], [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW]),
     ]
 }
--- macros/examples/resolve_edit.py
+++ macros/examples/resolve_edit.py
@@ -5,28 +5,30 @@ from sleep import Sleep
 
 app = {
     'name' : 'DV Resolve [Edit]',
-    'order': 1,
+    'order': 1,    
+    'launch': 
+        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.FOUR]),
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x000000, '<<', 'j'),
-        (0x000000, '>||', 'k'),
-        (0x000000, '>>', 'l' ),
+        (0x20660A, '- ', [Keycode.COMMAND, '-']),
+        (0x660556, 'Zoom', [Keycode.LEFT_SHIFT, 'z']),
+        (0x20660A, ' +', [Keycode.COMMAND, '=']),
         # 2nd row ----------
-        (0x000000, '- ', [Keycode.COMMAND, '-']),
-        (0x000000, 'Zoom', [Keycode.LEFT_SHIFT, 'z']),
-        (0x000000, ' +', [Keycode.COMMAND, '=']),
+        (0x663F00, '<<', 'j'),
+        (0x660556, '>||', Keycode.SPACE),
+        (0x663F00, '>>', 'l' ),
         # 3rd row ----------
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
-        (0x000000, '       ', []),
+        (0x663F00, 'Prev', Keycode.UP_ARROW),
+        (0x660556, ' [*] ', 'k'),
+        (0x663F00, 'Next', Keycode.DOWN_ARROW),
         # 4th row ----------
-        (0x000000, 'Select', 'a'),
-        (0x000000, 'Trim', 't'),
-        (0x000000, 'Blade', 'b'),
+        (0x663F00, 'In', 'i'),
+        (0x660556, 'Clear', 'x'),
+        (0x663F00, 'Out', 'o'),
         # Rotary Encoder ---
         (0x000000, None, Sleep()),
-        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW]),
-        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW]),
+        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW], [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW], [Keycode.LEFT_SHIFT, Keycode.LEFT_ARROW]),
+        (0x000000, None, [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW], [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW], [Keycode.LEFT_SHIFT, Keycode.RIGHT_ARROW]),
     ]
 }
