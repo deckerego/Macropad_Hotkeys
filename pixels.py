05d6f84637cf201c2179f0040731101fc8e19e33
John Ellis <532789+deckerego@users.noreply.github.com>
2026-02-13 14:06:49 -0500

    Added in animations

--- pixels.py
+++ pixels.py
@@ -1,3 +1,5 @@
+import time
+
 BRIGHTNESS = 0.3
 
 class Pixels:
@@ -25,10 +27,28 @@ class Pixels:
         self.pixels.show()
 
     def highlight(self, key_index, color):
-        self.pixels[key_index] = color
-        self.pixels.show()
+        for alpha in range(5):
+            color_blend = Pixels.blend(self.macros[key_index][0], color, alpha / 5.0)
+            self.pixels[key_index] = color_blend
+            self.pixels.show()
+            time.sleep(0.1)
 
     def reset(self, key_index):
         self.pixels[key_index] = self.macros[key_index][0]
         self.pixels.brightness = BRIGHTNESS
         self.pixels.show()
+
+    @staticmethod
+    def hexToTuple(color):
+        return (color >> 16, (color >> 8) & 0xFF, color & 0xFF)
+
+    @staticmethod
+    def blend(color_fg, color_bg, alpha_fg):
+        red_fg, green_fg, blue_fg = Pixels.hexToTuple(color_fg)
+        red_bg, green_bg, blue_bg = Pixels.hexToTuple(color_bg)
+        alpha_bg = 1.0 - alpha_fg
+        color_result_alpha = 1 - (1 - alpha_fg) * (1 - alpha_bg)
+        color_result_red = red_fg * alpha_fg / color_result_alpha + red_bg * alpha_bg * (1 - alpha_fg) / color_result_alpha
+        color_result_green = green_fg * alpha_fg / color_result_alpha + green_bg * alpha_bg * (1 - alpha_fg) / color_result_alpha
+        color_result_blue = blue_fg * alpha_fg / color_result_alpha + blue_bg * alpha_bg * (1 - alpha_fg) / color_result_alpha
+        return (color_result_red, color_result_green, color_result_blue)
